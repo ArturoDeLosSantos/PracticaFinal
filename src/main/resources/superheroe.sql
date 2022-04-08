e263d0a4924d393791bc80771998cd883c81b2e1
ArturoDeLosSantos <arturodlscuellar@gmail.com>
2022-04-08 23:10:37 +0200

    Nueva carga de datos SQL

---
+++ src/main/resources/superheroe.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.superheroe (id,estado,nombre) VALUES
+	 (1,'vivo','Superman'),
+	 (2,'vivo','Hulk'),
+	 (3,'muerto','Spiderman'),
+	 (4,'vivo','Super Lopez'),
+	 (5,'muerto','Catwoman'),
+	 (6,'vivo','Elektra'),
+	 (7,'muerto','Batman'),
+	 (8,'vivo','Thor');
